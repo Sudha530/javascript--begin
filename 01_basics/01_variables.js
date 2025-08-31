515d312e900f9866423f5d2c20a747a21555b6d9
Sudha530 <sudhasaroj718@gmail.com>
2025-08-31 22:16:47 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "hitest@google.com"
+var accountPassword ="12345"
+accountCity = "jaipur"
+let accountState; 
+  
+// accountId = 2 not aallowed 
+
+
+accountEmail ="hc@hv.com"
+accountPassword ="21223445"
+accountCity = "delhi"
+
+console.log(accountId);
+
+/*
+prefer not use var 
+because  of issue in block scope 
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity  ,accountState])
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3 + 3) //we are using node js,not browser
+
+console.log(3 + 3) //code readibilty should be high 
+console.log("sudha")
+
+let name = "sudha"
+let age = 22
+let isLoggedIn = false
+let state;
+
+
+
+
+// number =>2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => variable value not assigned
+//symbol => unique
+//object => key:value pair
+
+console.log(typeof "sudha")
+
+console.log(typeof 22)
+
+console.log(typeof true)
+
+//function => object
+
+console.log(typeof undefined); //undefined
+
+console.log(typeof null); //object =>bug in js
